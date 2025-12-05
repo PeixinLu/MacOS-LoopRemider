ff9d418dd2a5e0add9e4160c5c6e1895b51bbf2c
px <lupeixin@datatell.com.cn>
2025-12-05 10:47:59 +0800

    Initial Commit

---
+++ loopRemider/loopRemiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  loopRemiderApp.swift
+//  loopRemider
+//
+//  Created by 数源 on 2025/12/5.
+//
+
+import SwiftUI
+
+@main
+struct loopRemiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
